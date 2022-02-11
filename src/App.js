f73a50f605008bf69e99b6e1bd28c3a404c07406
minjung <ypd06021@naver.com>
2022-02-11 21:02:24 +0900

    axios import하고 예시코드 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return <h1>hello</h1>;
-}
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        setData(response.data);
+      });
+  };
+  //http://localhost:4000
+
+  // useEffect(() => {
+  //   //메인 페이지 접속시 useEffect로 axios호출해서 localhost:4000에서 데이터 받아오도록 하기 (특정 api 아무거나)
+  // })
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
