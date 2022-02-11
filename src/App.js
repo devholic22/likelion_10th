5e6af97dad25879d4f9fb2eeea1ede12afeeb6f5
minjung <ypd06021@naver.com>
2022-02-12 01:05:57 +0900

    useEffect 추가 수정, cors 에러 구현완료

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
+
+  useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .get("http://localhost:4000")
+      // .get("https://jsonplaceholder.typicode.com/todos/1")
       .then((response) => {
         setData(response.data);
       });
-  };
-  //http://localhost:4000
-
-  // useEffect(() => {
-  //   //메인 페이지 접속시 useEffect로 axios호출해서 localhost:4000에서 데이터 받아오도록 하기 (특정 api 아무거나)
-  // })
+  });
 
   return (
     <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
       {data && (
         <textarea
           rows={7}
