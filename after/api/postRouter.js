727614ce33d52e9743af6aa779ef52b55e50f1ac
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-05-09 20:01:26 +0900

    POST /api/posts - 글 생성 완료 및 GET /api/post - 글 개별 항목 조회 수정

--- after/api/postRouter.js
+++ after/api/postRouter.js
@@ -6,10 +6,11 @@ const postRouter = Router();
 // 글 개별 항목 조회
 const getOnePost = (req, res) => {
   const { id } = req.body;
-  const result = database.data.find((data) => data.id === id);
-
-  if (result) {
-    return res.json(result);
+  const result = database.data.findIndex((data) => data.id === id);
+  if (result != -1) {
+    const sorted = database.data.filter((data) => data.id === id)[0];
+    database.data = sorted;
+    return res.json(database);
   }
   return res.json({
     error: "Post not exist"
--- after/api/postsRouter.js
+++ after/api/postsRouter.js
@@ -8,6 +8,20 @@ const getAllPost = (req, res) => {
   return res.json(database);
 };
 
+// 글 생성
+const createPost = (req, res) => {
+  const { id, content, writer } = req.body;
+  database.data = {
+    post: { id }
+  };
+  return res.json(database);
+};
+
+// 특정 글 수정
+
+// 특정 글 삭제
+
 postsRouter.get("/", getAllPost);
+postsRouter.post("/", createPost);
 
 export default postsRouter;
