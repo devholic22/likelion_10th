98f08338d6833bc2315caf18a24b002c73f0b56d
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-05-09 18:50:31 +0900

    GET /api/posts - 글 목록 조회 완료

--- after/api/postsRouter.js
+++ after/api/postsRouter.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import database from "../database";
 
 const postsRouter = Router();
+
+// 글 목록 조회
+const getAllPost = (req, res) => {
+  return res.json(database);
+};
+
+postsRouter.get("/", getAllPost);
 
 export default postsRouter;
